cb590e4b0ef47faf8d0973e9ca6d9f7ef0c5c14a
afonso <afonso@home>
2017-02-20 18:10:36 +0000

    Initial project

---
+++ src/testgit/Hello.java
@@ -0,0 +1,16 @@
+package testgit;
+
+/**
+ *
+ * @author afonso
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    
+}
